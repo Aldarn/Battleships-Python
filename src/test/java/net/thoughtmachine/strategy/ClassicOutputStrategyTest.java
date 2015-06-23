12038ddd51efe762bd746c51fdb6b44a9caf7288
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-23 22:22:59 +0100

    Added classic output strategy tests.

---
+++ src/test/java/net/thoughtmachine/strategy/ClassicOutputStrategyTest.java
@@ -0,0 +1,22 @@
+package net.thoughtmachine.strategy;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ClassicOutputStrategyTest {
+	@Test
+	public void testGetOutput() {
+		fail();
+	}
+	
+	@Test
+	public void testGetOutputIncludesAllShips() {
+		fail();
+	}
+	
+	@Test
+	public void testGetOutputIndicatesSunkShips() {
+		fail();
+	}
+}
