5e14fe38c8be845adf3f1659738a5f35327a4c57
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-23 22:11:27 +0100

    Added input parser service impl tests.

---
+++ src/test/java/net/thoughtmachine/service/InputParserServiceImplTest.java
@@ -0,0 +1,52 @@
+package net.thoughtmachine.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class InputParserServiceImplTest {
+	@Test
+	public void testParsesBoardSize() {
+		fail();
+	}
+	
+	@Test
+	public void testInvalidBoardSizeThrowsException() {
+		fail();
+	}
+	
+	@Test
+	public void testParsesShips() {
+		fail();
+	}
+	
+	@Test
+	public void testInvalidShipsThrowsException() {
+		fail();
+	}
+	
+	@Test
+	public void testParsesShootOperation() {
+		fail();
+	}
+	
+	@Test
+	public void testParsesMoveOperation() {
+		fail();
+	}
+	
+	@Test
+	public void testNoOperationContainsShootAndMove() {
+		fail();
+	}
+	
+	@Test
+	public void testInvalidOperationCoordinatesThrowsException() {
+		fail();
+	}
+	
+	@Test
+	public void testMalformedOperationThrowsException() {
+		fail();
+	}
+}
