a93bc79ce97cfd6e140e056c03d4430f946deb19
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-23 22:16:51 +0100

    Added output service impl test.

---
+++ src/test/java/net/thoughtmachine/service/OutputServiceImplTest.java
@@ -0,0 +1,12 @@
+package net.thoughtmachine.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class OutputServiceImplTest {
+	@Test
+	public void testCreateOutput() {
+		fail();
+	}
+}
