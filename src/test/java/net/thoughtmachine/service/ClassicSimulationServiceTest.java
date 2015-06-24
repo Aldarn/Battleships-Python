1a07f5aeaa88019792a9b10cb263c5e903ee8a8d
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-24 20:39:19 +0100

    Added tests for location sharing with wrecks to ClassicSimulationServiceTest.

--- src/test/java/net/thoughtmachine/service/ClassicSimulationServiceTest.java
+++ src/test/java/net/thoughtmachine/service/ClassicSimulationServiceTest.java
@@ -22,7 +22,19 @@ public class ClassicSimulationServiceTest {
 		fail();
 	}
 	
-	public void testOperationEndShipsSharingLocationThrowsException() {
+	/**
+	 * Test that at the end of an operation a ship can share the location 
+	 * of other ships providing all the other ships have been wrecked.
+	 */
+	public void testOperationEndShipCanShareLocationWithWrecks() {
+		fail();
+	}
+	
+	/**
+	 * Test that at the end of an operation a ship cannot share the location 
+	 * of other ships that have not been wrecked.
+	 */
+	public void testOperationEndNonWreckShipsSharingLocationThrowsException() {
 		fail();
 	}
 }
