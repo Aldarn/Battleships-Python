cf7c0d43d79e19fdc84b5944a9a21cbcf74c3367
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-24 22:55:01 +0100

    First pass implementation of main class.

--- src/main/java/net/thoughtmachine/BlackSails.java
+++ src/main/java/net/thoughtmachine/BlackSails.java
@@ -1,27 +1,71 @@
 package net.thoughtmachine;
 
-import java.io.*;
+import java.io.InputStream;
+
+import net.thoughtmachine.domain.GameBoard;
+import net.thoughtmachine.domain.GameModel;
+import net.thoughtmachine.service.ClassicInputParserService;
+import net.thoughtmachine.service.ClassicSimulationService;
+import net.thoughtmachine.service.FileOutputService;
+import net.thoughtmachine.service.InputParserService;
+import net.thoughtmachine.service.OutputService;
+import net.thoughtmachine.service.SimulationService;
+import net.thoughtmachine.strategy.ClassicOutputStrategy;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class BlackSails {
+	InputParserService inputParser;
+	SimulationService simulationService;
+	OutputService outputService;
+	private String inputPath;
+	
+	public BlackSails(InputParserService inputParser, SimulationService simulationService,
+			OutputService outputService, String inputPath) {
+		this.inputParser = inputParser;
+		this.simulationService = simulationService;
+		this.outputService = outputService;
+		this.inputPath = inputPath;
+	}
+	
+	public void sail() {
+		// Parse the input and get the game model
+		GameModel gameModel = inputParser.parseInput(openInputStream(inputPath));
+		
+		// Simulate the model
+		GameBoard finalGameBoard = simulationService.play(gameModel);
+		
+		// Output the results
+		outputService.createOutput(new ClassicOutputStrategy(), finalGameBoard);
+	}
+  
+  	public InputStream openInputStream(String filePath) {
+  		return getClass().getResourceAsStream(filePath);
+  	}
+  	
+  	public static CommandLine getCommandLineArguments(String... args) throws ParseException {
+  		Options options = new Options();
+  		options.addOption("f", false, "path to input file");
+  		
+  		CommandLineParser parser = new DefaultParser();
+  		return parser.parse(options, args);
+  	}
 
-  private static final String input = "/input.txt";
-
-  public void loadInput() {
-    InputStream is = getClass().getResourceAsStream(input);
-    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-
-    try {
-      String line;
-      while ((line = reader.readLine()) != null) {
-        System.out.println(line);
-      }
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+  	public static void main(String... args) throws ParseException {
+  		// Create the services... this could be done by dependency injection
+		InputParserService inputParser = new ClassicInputParserService();
+		SimulationService simulationService = new ClassicSimulationService();
+		OutputService outputService = new FileOutputService();
+		
+		// Get command line arguments
+		CommandLine clArgs = getCommandLineArguments(args);
+		
+		// Let's go!
+  		BlackSails app = new BlackSails(inputParser, simulationService, outputService, clArgs.getOptionValue('f'));
+  		app.sail();
     }
-  }
-
-  public static void main(String... args) {
-    BlackSails app = new BlackSails();
-    app.loadInput();
-  }
 }
