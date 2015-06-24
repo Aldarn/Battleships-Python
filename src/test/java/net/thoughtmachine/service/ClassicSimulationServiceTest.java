09c7983d3987f21480fa5c55285f624ac4a13b78
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-24 19:50:54 +0100

    Renamed SimulationServiceImplTest and InputParserServiceImplTest to Classic.

--- src/test/java/net/thoughtmachine/service/ClassicSimulationServiceTest.java
+++ src/test/java/net/thoughtmachine/service/ClassicSimulationServiceTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-public class SimulationServiceImplTest {
+public class ClassicSimulationServiceTest {
 	@Test
 	public void testPlaySimulation() {
 		fail();
