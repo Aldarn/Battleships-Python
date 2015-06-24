1d69c3bdd2facb8ed2dc687bb689dd259bad3b52
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-24 19:18:17 +0100

    Added simulation service tests.

---
+++ src/test/java/net/thoughtmachine/service/SimulationServiceImplTest.java
@@ -0,0 +1,28 @@
+package net.thoughtmachine.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class SimulationServiceImplTest {
+	@Test
+	public void testPlaySimulation() {
+		fail();
+	}
+	
+	public void testMoveOperationsApplyToBoard() {
+		fail();
+	}
+	
+	public void testTurnOperationsDontApplyToBoard() {
+		fail();
+	}
+	
+	public void testShootOperationsDontApplyToBoard() {
+		fail();
+	}
+	
+	public void testOperationEndShipsSharingLocationThrowsException() {
+		fail();
+	}
+}
