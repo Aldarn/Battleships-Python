123521a6f464e1b66012183a2b75831790a50d2e
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-24 19:18:05 +0100

    Added turn action tests.

---
+++ src/test/java/net/thoughtmachine/action/TurnActionTest.java
@@ -0,0 +1,17 @@
+package net.thoughtmachine.action;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TurnActionTest {
+	@Test
+	public void testTurnActionLeft() {
+		fail();
+	}
+	
+	@Test
+	public void testMoveActionRight() {
+		fail();
+	}
+}
