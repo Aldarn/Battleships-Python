a88d9180a0cf8b4a8c5b98ccc7616c71cce4508a
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-25 00:37:48 +0100

    Added integration test for example input.

--- src/integration-test/java/net/thoughtmachine/BlackSailsIntegrationTest.java
+++ src/integration-test/java/net/thoughtmachine/BlackSailsIntegrationTest.java
@@ -1,13 +1,28 @@
 package net.thoughtmachine;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.commons.cli.ParseException;
 import org.junit.Test;
 
 public class BlackSailsIntegrationTest {
 	@Test
-	public void testInputProvidesCorrectOutput() {
-		fail();
+	public void testInputProvidesCorrectOutput() throws ParseException, IOException {
+		// -------------------------------------------------------
+		BlackSails.main("-f input.txt");
+		// -------------------------------------------------------
+		File file = new File("output.txt");
+		FileInputStream fis = new FileInputStream(file);
+		byte[] data = new byte[(int) file.length()];
+		fis.read(data);
+		fis.close();
+		
+		assertEquals(String.valueOf(data), "(1, 3, N)\n(9, 2, E) SUNK");
 	}
 	
 	@Test
