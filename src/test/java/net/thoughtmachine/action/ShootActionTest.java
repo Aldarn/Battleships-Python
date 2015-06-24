696122161e2ade28ba271f38a3bf2f414f33ce9c
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-24 19:17:56 +0100

    Added shoot action tests.

---
+++ src/test/java/net/thoughtmachine/action/ShootActionTest.java
@@ -0,0 +1,17 @@
+package net.thoughtmachine.action;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ShootActionTest {
+	@Test
+	public void testShootActionMiss() {
+		fail();
+	}
+	
+	@Test
+	public void testShootActionHit() {
+		fail();
+	}
+}
