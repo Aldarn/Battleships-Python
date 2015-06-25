f4bc1206801bc903eba2320eac7dfcd92f8511aa
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-25 19:15:08 +0100

    Added FileOutputService tests.

--- src/main/java/net/thoughtmachine/service/FileOutputService.java
+++ src/main/java/net/thoughtmachine/service/FileOutputService.java
@@ -7,9 +7,11 @@ import net.thoughtmachine.domain.GameBoard;
 import net.thoughtmachine.strategy.OutputStrategy;
 
 public class FileOutputService implements OutputService {
+	public static String outputFileName = "output.txt";
+	
 	@Override
 	public void createOutput(OutputStrategy strategy, GameBoard board) throws IOException {
-		FileOutputStream out = new FileOutputStream("output.txt");
+		FileOutputStream out = new FileOutputStream(outputFileName);
 		out.write(strategy.generateOutput(board).getBytes());
 		out.close();
 	}
