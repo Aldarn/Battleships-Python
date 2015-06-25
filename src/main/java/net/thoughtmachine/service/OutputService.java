fda5c8f05faeccfb5162bcf5f35c3302d8805036
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-25 01:31:31 +0100

    Exception.

--- src/main/java/net/thoughtmachine/service/OutputService.java
+++ src/main/java/net/thoughtmachine/service/OutputService.java
@@ -1,8 +1,10 @@
 package net.thoughtmachine.service;
 
+import java.io.IOException;
+
 import net.thoughtmachine.domain.GameBoard;
 import net.thoughtmachine.strategy.OutputStrategy;
 
 public interface OutputService {
-	void createOutput(OutputStrategy strategy, GameBoard board);
+	void createOutput(OutputStrategy strategy, GameBoard board) throws IOException;
 }
