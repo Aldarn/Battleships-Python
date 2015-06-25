4f93baa7c80dcc5694eca8907ce6efc29017ceb6
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-25 01:23:28 +0100

    Refactored input parser service and added operation parsing.

--- src/main/java/net/thoughtmachine/service/ClassicInputParserService.java
+++ src/main/java/net/thoughtmachine/service/ClassicInputParserService.java
@@ -9,18 +9,37 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import net.thoughtmachine.action.Action;
+import net.thoughtmachine.action.MoveAction;
+import net.thoughtmachine.action.ShootAction;
+import net.thoughtmachine.action.TurnAction;
 import net.thoughtmachine.domain.Coord;
+import net.thoughtmachine.domain.Direction;
 import net.thoughtmachine.domain.GameBoard;
 import net.thoughtmachine.domain.GameModel;
 import net.thoughtmachine.domain.Orientation;
 import net.thoughtmachine.domain.Ship;
 
+/**
+ * Parses the input according to the spec.
+ * 
+ * TODO: This is pretty messy and should be refactored. Let's assume this is a production MVP??
+ */
 public class ClassicInputParserService implements InputParserService {
 	private static final int BOARD_SIZE_LINE = 0;
 	private static final int SHIPS_LINE = 1;
 	
 	private static final Pattern SHIP_PATTERN = Pattern.compile("(\\(\\d+, \\d+, \\w\\))\\s*");
 	private static final Pattern SHIP_VALUES_PATTERN = Pattern.compile("\\((\\d+), (\\d+), (\\w)\\)");
+	private static final Pattern OPERATION_PATTERN = Pattern.compile("\\(\\d+, \\d\\))\\s*(\\w+)*");
+	private static final Pattern OPERATION_COORD_PATTERN = Pattern.compile("\\((\\d+), (\\d+)\\)");
+	
+	private static final int OPERATION_SHOOT_GROUP_COUNT = 1;
+	private static final int OPERATION_OTHER_GROUP_COUNT = 2;
+	
+	private static final char ACTION_MOVE_TOKEN = 'M';
+	private static final char ACTION_TURN_LEFT_TOKEN = 'L';
+	private static final char ACTION_TURN_RIGHT_TOKEN = 'R';
 	
 	@Override
 	public GameModel parseInput(InputStream inputStream) {
@@ -41,7 +60,7 @@ public class ClassicInputParserService implements InputParserService {
 	    				loadShips(initialBoard, line);
 	    				break;
 	    			default:
-	    				loadOperation(line);
+	    				loadOperation(gameModel, line);
 	    		}
 	    		
 	    		lineNumber++;
@@ -72,18 +91,32 @@ public class ClassicInputParserService implements InputParserService {
 			for(int i = 1; i < shipMatcher.groupCount(); i++) {
 				Matcher shipValuesMatcher = SHIP_VALUES_PATTERN.matcher(shipMatcher.group(i));
 				if(shipValuesMatcher.matches()) {
-					Coord coord = new Coord(Integer.valueOf(shipValuesMatcher.group(1)), 
-						Integer.valueOf(shipValuesMatcher.group(1)));
+					Coord coord = parseCoord(board, shipValuesMatcher.group(1), shipValuesMatcher.group(2));
 					if(board.get(coord) != null) {
 						throw new RuntimeException("Two ships found starting in the same location (" + coord + ").");
-					} else {
-						List<Ship> shipSet = new ArrayList<Ship>();
-						shipSet.add(new Ship(coord, getOrientationFromString(ships)));
-						board.put(coord, shipSet);
 					}
+					List<Ship> shipSet = new ArrayList<Ship>();
+					shipSet.add(parseShip(coord, shipValuesMatcher.group(3)));
+					board.put(coord, shipSet);
 				}
 			}
 		}
+	}
+	
+	private Coord parseCoord(GameBoard board, String coordX, String coordY) {
+		int x = Integer.valueOf(coordX);
+		int y = Integer.valueOf(coordY);
+		
+		// Check if the coordinate falls outside the board
+		if(x < 0 || y < 0 || x > board.getSize() || y > board.getSize()) {
+			throw new RuntimeException("Coordinate '" + coordX + ", " + coordY + "'found outisde of board range (0, " + board.getSize() + ")");
+		}
+		
+		return new Coord(x, y);			
+	}
+	
+	private Ship parseShip(Coord coord, String orientationString) {
+		return new Ship(coord, getOrientationFromString(orientationString));
 	}
 	
 	private Orientation getOrientationFromString(String orientationString) {
@@ -94,7 +127,45 @@ public class ClassicInputParserService implements InputParserService {
 		return orientation;
 	}
 	
-	private void loadOperation(String operation) {
-		
+	/**
+	 * Parses out an individual operation.
+	 * 
+	 * TODO: This needs refactoring, so much coupling...
+	 */
+	private void loadOperation(GameModel gameModel, String operationString) {
+		Matcher operationMatcher = OPERATION_PATTERN.matcher(operationString);
+		if(operationMatcher.matches()) {
+			Matcher operationCoordMatcher = OPERATION_COORD_PATTERN.matcher(operationMatcher.group(1));
+			Coord coord = parseCoord(gameModel.getInitialBoard(), operationCoordMatcher.group(1), operationCoordMatcher.group(2));
+			
+			switch(operationMatcher.groupCount()) {
+				case OPERATION_SHOOT_GROUP_COUNT:	
+					List<Action> shootActionList = new ArrayList<>();
+					shootActionList.add(new ShootAction(coord));
+					gameModel.add(shootActionList);
+					break;
+				case OPERATION_OTHER_GROUP_COUNT:
+					List<Action> moveActionList = new ArrayList<>();
+					for(char actionToken : operationMatcher.group(2).toCharArray()) {
+						switch(actionToken) {
+							case ACTION_MOVE_TOKEN:
+								moveActionList.add(new MoveAction(coord));
+								break;
+							case ACTION_TURN_LEFT_TOKEN:
+								moveActionList.add(new TurnAction(coord, Direction.LEFT));
+								break;
+							case ACTION_TURN_RIGHT_TOKEN:
+								moveActionList.add(new TurnAction(coord, Direction.RIGHT));
+								break;
+							default:
+								throw new RuntimeException("Could not determine action from token '" + actionToken + "'.");
+						}
+					}
+					gameModel.add(moveActionList);			
+					break;
+				default:
+					throw new RuntimeException("Could not determine operation from string '" + operationString + "'.");
+			}
+		}
 	}
 }
