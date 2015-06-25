ac9db7c91f8c81cdc0af9539764b45f5472de407
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-25 01:22:51 +0100

    Coord now has toString method and equals method based on x and y.

--- src/main/java/net/thoughtmachine/domain/Coord.java
+++ src/main/java/net/thoughtmachine/domain/Coord.java
@@ -1,5 +1,8 @@
 package net.thoughtmachine.domain;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 /**
  * Pair representing a coordinate on the game board.
  */
@@ -10,5 +13,32 @@ public class Coord {
 	public Coord(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 31).
+			append(x).
+			append(y).
+			toHashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Coord))
+			return false;
+		if (obj == this)
+			return true;
+
+		Coord rhs = (Coord) obj;
+		return new EqualsBuilder().
+			append(x, rhs.x).
+			append(y, rhs.y).
+			isEquals();		
+	}
+	
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ")";
 	}
 }
