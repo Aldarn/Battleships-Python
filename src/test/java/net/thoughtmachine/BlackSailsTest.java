b0d0efe915f5cf616ba0eb695add3944c2c24932
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-25 16:30:34 +0100

    Implemented black sails app test.

--- src/test/java/net/thoughtmachine/BlackSailsTest.java
+++ src/test/java/net/thoughtmachine/BlackSailsTest.java
@@ -1,17 +1,43 @@
 package net.thoughtmachine;
 
-import static org.junit.Assert.fail;
+import static org.mockito.Mockito.doReturn;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import net.thoughtmachine.domain.GameBoard;
+import net.thoughtmachine.domain.GameModel;
+import net.thoughtmachine.service.InputParserService;
+import net.thoughtmachine.service.OutputService;
+import net.thoughtmachine.service.SimulationService;
+import net.thoughtmachine.strategy.OutputStrategy;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class BlackSailsTest {
 	@Test
-	public void testGameLoop() {
-		// TODO: Verify parsing service is used
-		// TODO: Verify game modal is played
-		// TODO: Verify output is generated
-		// TODO: Assert results of each step are as expected
+	public void testGameLoop() throws IOException {
+		InputParserService inputParser = Mockito.mock(InputParserService.class);
+		SimulationService simulationService = Mockito.mock(SimulationService.class);
+		OutputService outputService = Mockito.mock(OutputService.class);
 		
-		fail();
+		BlackSails app = new BlackSails(inputParser, simulationService, outputService, "/input.txt");
+		BlackSails appSpy = Mockito.spy(app);
+		InputStream inputStream = Mockito.mock(InputStream.class);
+		doReturn(inputStream).when(appSpy).openInputStream("/input.txt");
+		
+		// TODO: Should use concrete instances of domain objects when testing
+		GameModel model = Mockito.mock(GameModel.class);
+		GameBoard board = Mockito.mock(GameBoard.class);
+		
+		Mockito.when(inputParser.parseInput(inputStream)).thenReturn(model);
+		Mockito.when(simulationService.play(model)).thenReturn(board);
+		// -------------------------------------------------------
+		appSpy.sail();
+		// -------------------------------------------------------
+		Mockito.verify(inputParser).parseInput(inputStream);
+		Mockito.verify(simulationService).play(model);
+		Mockito.verify(outputService).createOutput((OutputStrategy)Mockito.anyObject(), (GameBoard)Mockito.anyObject());
 	}
 }
