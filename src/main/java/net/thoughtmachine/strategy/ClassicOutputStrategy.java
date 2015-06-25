57014747a65223f67cd69e6293c1c5c3ea7392bb
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-25 02:44:55 +0100

    Implemented ClassicOutputStrategy.

--- src/main/java/net/thoughtmachine/strategy/ClassicOutputStrategy.java
+++ src/main/java/net/thoughtmachine/strategy/ClassicOutputStrategy.java
@@ -1,10 +1,19 @@
 package net.thoughtmachine.strategy;
 
+import java.util.List;
+
 import net.thoughtmachine.domain.GameBoard;
+import net.thoughtmachine.domain.Ship;
 
 public class ClassicOutputStrategy implements OutputStrategy {
 	@Override
 	public String generateOutput(GameBoard board) {
-		return null;
+		String output = "";
+		for(List<Ship> ships : board.values()) {
+			for(Ship ship : ships) {
+				output += ship + "\n";
+			}
+		}
+		return output.substring(0, -2);
 	}
 }
