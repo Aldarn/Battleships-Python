351c70401d69875542e2afa37bd45f9a488487fa
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-25 16:19:01 +0100

    Added validation to shoot action.

--- src/main/java/net/thoughtmachine/action/ShootAction.java
+++ src/main/java/net/thoughtmachine/action/ShootAction.java
@@ -7,7 +7,13 @@ import net.thoughtmachine.domain.Ship;
 public class ShootAction extends Action {
 	@Override
 	public Coord act(GameBoard board, Ship ship, Coord coord) {
-		ship.sink();
+		if(ship != null) {
+			if(ship.isSunk()) {
+				throw new RuntimeException("Tried to sink already sunk ship" + ship + ".");
+			} else {
+				ship.sink();
+			}
+		}
 		return coord;
 	}
 }
