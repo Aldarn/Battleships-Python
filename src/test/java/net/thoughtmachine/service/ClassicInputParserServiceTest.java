33dae79cd7cbd93a649a550dfa0843662785da47
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-06-25 15:13:30 +0100

    Finished some more input parser service tests.

--- src/test/java/net/thoughtmachine/service/ClassicInputParserServiceTest.java
+++ src/test/java/net/thoughtmachine/service/ClassicInputParserServiceTest.java
@@ -1,6 +1,7 @@
 package net.thoughtmachine.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
@@ -12,6 +13,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import net.thoughtmachine.action.MoveAction;
+import net.thoughtmachine.action.ShootAction;
+import net.thoughtmachine.action.TurnAction;
+import net.thoughtmachine.domain.Coord;
+import net.thoughtmachine.domain.Direction;
 import net.thoughtmachine.domain.GameModel;
 import net.thoughtmachine.domain.Orientation;
 import net.thoughtmachine.domain.Ship;
@@ -36,6 +42,7 @@ public class ClassicInputParserServiceTest {
 	
 	@Test(expected = RuntimeException.class)
 	public void testInvalidBoardSizeThrowsException() throws IOException {
+		// TODO: This should be less brittle and test a more specific exception is thrown
 		ClassicInputParserService parserSpy = spy(parser);
 		BufferedReader reader = mock(BufferedReader.class);
 		InputStream inputStream = mock(InputStream.class);
@@ -75,6 +82,7 @@ public class ClassicInputParserServiceTest {
 	
 	@Test(expected = RuntimeException.class)
 	public void testShipUnknownOrientationThrowsException() throws IOException {
+		// TODO: This should be less brittle and test a more specific exception is thrown
 		ClassicInputParserService parserSpy = spy(parser);
 		BufferedReader reader = mock(BufferedReader.class);
 		InputStream inputStream = mock(InputStream.class);
@@ -85,23 +93,66 @@ public class ClassicInputParserServiceTest {
 		// -------------------------------------------------------
 	}
 	
-	@Test
-	public void testShipCoordBeyondBoardSizeThrowsException() {
-		fail();
+	@Test(expected = RuntimeException.class)
+	public void testShipCoordBeyondBoardSizeThrowsException() throws IOException {
+		// TODO: This should be less brittle and test a more specific exception is thrown
+		ClassicInputParserService parserSpy = spy(parser);
+		BufferedReader reader = mock(BufferedReader.class);
+		InputStream inputStream = mock(InputStream.class);
+		doReturn(reader).when(parserSpy).getReader(inputStream);
+		when(reader.readLine()).thenReturn("5").thenReturn("(6, 5, E)").thenReturn(null);
+		// -------------------------------------------------------
+		parserSpy.parseInput(inputStream);
+		// -------------------------------------------------------
 	}
 	
 	@Test
-	public void testParsesShootOperation() {
-		fail();
+	public void testParsesShootOperation() throws IOException {
+		ClassicInputParserService parserSpy = spy(parser);
+		BufferedReader reader = mock(BufferedReader.class);
+		InputStream inputStream = mock(InputStream.class);
+		doReturn(reader).when(parserSpy).getReader(inputStream);
+		when(reader.readLine()).thenReturn("10").thenReturn("(1, 2, N)").thenReturn("(1, 2)").thenReturn(null);
+		// -------------------------------------------------------
+		GameModel model = parserSpy.parseInput(inputStream);
+		// -------------------------------------------------------
+		assertEquals(model.size(), 1);
+		assertEquals(model.get(0).getCoord(), new Coord(1, 2));
+		assertEquals(model.get(0).size(), 1);
+		assertTrue(model.get(0).get(0) instanceof ShootAction);
 	}
 	
 	@Test
-	public void testParsesMoveOperation() {
-		fail();
+	public void testParsesMoveOperation() throws IOException {
+		ClassicInputParserService parserSpy = spy(parser);
+		BufferedReader reader = mock(BufferedReader.class);
+		InputStream inputStream = mock(InputStream.class);
+		doReturn(reader).when(parserSpy).getReader(inputStream);
+		when(reader.readLine()).thenReturn("10").thenReturn("(3, 4, S)").thenReturn("(3, 4) MRL").thenReturn(null);
+		// -------------------------------------------------------
+		GameModel model = parserSpy.parseInput(inputStream);
+		// -------------------------------------------------------
+		assertEquals(model.size(), 1);
+		assertEquals(model.get(0).getCoord(), new Coord(3, 4));
+		assertEquals(model.get(0).size(), 3);
+		assertTrue(model.get(0).get(0) instanceof MoveAction);
+		assertTrue(model.get(0).get(1) instanceof TurnAction);
+		assertEquals(((TurnAction)model.get(0).get(1)).getDirection(), Direction.RIGHT);
+		assertTrue(model.get(0).get(2) instanceof TurnAction);
+		assertEquals(((TurnAction)model.get(0).get(2)).getDirection(), Direction.LEFT);
 	}
 	
 	@Test
-	public void testNoOperationContainsShootAndMove() {
+	public void testNoOperationContainsShootAndMove() throws IOException {
+		ClassicInputParserService parserSpy = spy(parser);
+		BufferedReader reader = mock(BufferedReader.class);
+		InputStream inputStream = mock(InputStream.class);
+		doReturn(reader).when(parserSpy).getReader(inputStream);
+		when(reader.readLine()).thenReturn("10").thenReturn("(3, 4, S)").thenReturn("(3, 4) MRL")
+			.thenReturn("(");
+		// -------------------------------------------------------
+		GameModel model = parserSpy.parseInput(inputStream);
+		// -------------------------------------------------------
 		fail();
 	}
 	
